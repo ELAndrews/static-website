38ec0674c1e2b27b32b9420aa80ff990afec5307
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2019-12-19 10:32:33 +0000

    eslint dependancy

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+// npm init is use to initalise npm project and give a package.json.
+// --y flag will answer yes to all default answers
+// npm install live-server -D the -D makes it a dev dependency
+
+//ADDITIONAL COMMAND LINE WORK BELOW WITH REPLY
+//(base) Emmas-MBP:StaticProject emma$ npx eslint --init
+// npx: installed 123 in 7.522s
+// ? How would you like to use ESLint? To check syntax and find problems
+// ? What type of modules does your project use? JavaScript modules (import/
+// export)
+// ? Which framework does your project use? React
+// ? Does your project use TypeScript? No
+// ? Where does your code run? Browser
+// ? What format do you want your config file to be in? JavaScript
+// Local ESLint installation not found.
+// The config that you've selected requires the following dependencies:
+
+// eslint-plugin-react@latest eslint@latest
+// ? Would you like to install them now with npm? Yes
+// Installing eslint-plugin-react@latest, eslint@latest
+// npm WARN staticproject@1.0.0 No description
+
+// + eslint@6.7.2
+// + eslint-plugin-react@7.17.0
+// added 146 packages from 94 contributors and audited 2616 packages in 13.402s
+// found 0 vulnerabilities
+
+// Warning: React version not specified in eslint-plugin-react settings. See https://github.com/yannickcr/eslint-plugin-react#configuration .
+// Successfully created .eslintrc.js file in /Users/emma/Documents/Coding/Lambda/WebApplicationsII/StaticProject
+// ESLint was installed locally. We recommend using this local copy instead of your globally-installed copy.
+
 console.log(`this is working`);
 const greet = document.querySelector(".hello");
 greet.textContent = "Script is AWESOME!";
+
+const foo = "bar";
